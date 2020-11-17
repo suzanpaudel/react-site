46fd70cf79e63a001d0a71dc1cb3eed4963181cb
Sujan Paudel <sujan2000paudel@gmail.com>
2020-11-17 20:50:05 +0545

    Make Changes in the Home Page by using Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './tailwind.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Counter from './component/Counter';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Home from './views/Home';
@@ -10,25 +9,27 @@ import Product from './views/Product';
 
 function App() {
   return (
-    <Router>
-      <Header />
+    <div className='relative pb-10 min-h-screen'>
+      <Router>
+        <Header />
 
-      <div className='p-3'>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/products/:id'>
-            <Product />
-          </Route>
-        </Switch>
-      </div>
+        <div className='p-3'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/products/:id'>
+              <Product />
+            </Route>
+          </Switch>
+        </div>
 
-      <Footer />
-    </Router>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import HelloWorld from '../component/HelloWorld';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Loader from '../component/Loader';
+import ProductCard from '../component/ProductCard';
 
 const Home = () => {
+  const url =
+    'https://5fb3b881b6601200168f7c43.mockapi.io/products?page=1&limit=10';
+
+  const [products, setProducts] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
+
+  let content = null;
+
+  useEffect(() => {
+    setProducts({
+      loading: true,
+      data: null,
+      error: false,
+    });
+    axios
+      .get(url)
+      .then((response) => {
+        setProducts({
+          loading: false,
+          data: response.data,
+          error: false,
+        });
+      })
+      .catch((error) => {
+        setProducts({
+          loading: false,
+          data: null,
+          error: true,
+        });
+      });
+  }, [url]);
+
+  if (products.error) {
+    content = <h1>The product does not exist</h1>;
+  }
+
+  if (products.loading) {
+    content = <Loader />;
+  }
+
+  if (products.data) {
+    content = products.data.map((product) => {
+      return <ProductCard product={product} key={product.id} />;
+    });
+  }
+
   return (
     <div>
-      <h1 className='font-bold text-2xl'>This is the home page</h1>
-      <HelloWorld name='Sujan' />
+      <h1 className='font-bold text-2xl'>Best Sellers</h1>
+      {content}
     </div>
   );
 };
